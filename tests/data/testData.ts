6f6854818fcf72817cdcbe4a8522adca7fd860a4
Julia Selma Acevedo <juliaselma@gmail.com>
2025-10-26 13:23:35 +0100

    test data refactor

--- tests/data/testData.ts
+++ tests/data/testData.ts
@@ -1,13 +1,12 @@
 interface WorkflowData {
     DATASET_STORES: string;
     DATASET_STATES: string;
-    FILTER_STATE: string; // 'California'
+    FILTER_STATE: string; 
     COMPONENT_FILTER: string;
     COMPONENT_SPATIAL_FILTER: string;
     COMPONENT_MAP: string;
     MAP_NAME_POSITIVE: string;
     MAP_NAME_NEGATIVE: string;
-    // Nombres de los nodos
     NODE_STORES: string;
     NODE_STATES: string;
     NODE_SIMPLE_FILTER: string;
@@ -16,23 +15,14 @@ interface WorkflowData {
 }
 
 export const testData: WorkflowData = {
-    // Datasets
     DATASET_STORES: 'retail_stores',
     DATASET_STATES: 'usa_states_boundaries',
-    
-    // Filtros
     FILTER_STATE: 'California',
-    
-    // Nombres de componentes (en el panel lateral)
     COMPONENT_FILTER: 'Simple Filter',
     COMPONENT_SPATIAL_FILTER: 'Spatial Filter',
     COMPONENT_MAP: 'Create Builder Map',
-    
-    // Nombres de los mapas
     MAP_NAME_POSITIVE: 'Retail Stores by Filtered State',
     MAP_NAME_NEGATIVE: 'Retail_Stores_Negative_Match_Test',
-
-    // Nombres de los nodos en el canvas (si son diferentes al componente)
     NODE_STORES: 'retail_stores',
     NODE_STATES: 'usa_states_boundaries',
     NODE_SIMPLE_FILTER: 'Simple Filter',
