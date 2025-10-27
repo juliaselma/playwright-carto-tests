156549815b7a65a13b45e10ca1f6f7d6d27456a3
Julia Selma Acevedo <juliaselma@gmail.com>
2025-10-27 21:26:29 +0100

    Add Playwright CI/CD workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+/*import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  reporter: 'html',
+  //reporter: 'html',
   timeout: 240 * 1000,
 
   use: {
@@ -23,12 +23,14 @@ export default defineConfig({
       name: 'ui-chromium',
       testDir: './tests/ui/',
       use: { ...devices['Desktop Chrome'], headless: false },
+      reporter: [['html', { outputFolder: 'playwright-report/chromium' }]],
     },
     {
       name: 'ui-firefox',
       testDir: './tests/ui/',
 
       use: { ...devices['Desktop Firefox'], headless: false },
+      reporter: [['html', { outputFolder: 'playwright-report/firefox' }]],
     },
 
     // 2. API Mode
@@ -36,6 +38,55 @@ export default defineConfig({
       name: 'api-mode',
       testDir: './tests/api/',
       workers: 1,
+      use: {
+        baseURL: 'https://demoqa.com',
+      },
+      reporter: [['html', { outputFolder: 'playwright-report/api' }]],
+    },
+  ],
+});*/
+// playwright.config.ts (VERSION CORREGIDA)
+
+import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: 'html',
+  outputDir: 'playwright-report',
+  timeout: 240 * 1000,
+  workers: 2,
+
+  use: {
+    baseURL: 'https://carto.com/',
+    trace: 'on-first-retry',
+    actionTimeout: 90 * 1000,
+  },
+
+  projects: [
+    // 1. UI Mode (Chromium)
+    {
+      name: 'ui-chromium',
+      testDir: './tests/ui/',
+      use: { ...devices['Desktop Chrome'], headless: false },
+    },
+
+    // 1. UI Mode (Firefox)
+    {
+      name: 'ui-firefox',
+      testDir: './tests/ui/',
+      use: { ...devices['Desktop Firefox'], headless: false },
+    },
+
+    // 2. API Mode
+    {
+      name: 'api-mode',
+      testDir: './tests/api/',
       use: {
         baseURL: 'https://demoqa.com',
       },
