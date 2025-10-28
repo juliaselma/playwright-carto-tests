ebf2d0a19cb741b7855ea9b5505a15fb21dfd5e3
Julia Selma Acevedo <juliaselma@gmail.com>
2025-10-28 11:53:47 +0100

    Update playwright.config.ts to handle headless in local and CI CD

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,56 +1,9 @@
-/*import { defineConfig, devices } from '@playwright/test';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-export default defineConfig({
-  testDir: './tests',
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  //reporter: 'html',
-  timeout: 240 * 1000,
-
-  use: {
-    baseURL: 'https://carto.com/',
-    trace: 'on-first-retry',
-    actionTimeout: 90 * 1000,
-  },
-
-  projects: [
-    // 1. UI Mode
-    {
-      name: 'ui-chromium',
-      testDir: './tests/ui/',
-      use: { ...devices['Desktop Chrome'], headless: false },
-      reporter: [['html', { outputFolder: 'playwright-report/chromium' }]],
-    },
-    {
-      name: 'ui-firefox',
-      testDir: './tests/ui/',
-
-      use: { ...devices['Desktop Firefox'], headless: false },
-      reporter: [['html', { outputFolder: 'playwright-report/firefox' }]],
-    },
-
-    // 2. API Mode
-    {
-      name: 'api-mode',
-      testDir: './tests/api/',
-      workers: 1,
-      use: {
-        baseURL: 'https://demoqa.com',
-      },
-      reporter: [['html', { outputFolder: 'playwright-report/api' }]],
-    },
-  ],
-});*/
-// playwright.config.ts (VERSION CORREGIDA)
-
 import { defineConfig, devices } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+const isCI = !!process.env.CI;
 
 export default defineConfig({
   testDir: './tests',
@@ -73,14 +26,14 @@ export default defineConfig({
     {
       name: 'ui-chromium',
       testDir: './tests/ui/',
-      use: { ...devices['Desktop Chrome'], headless: true },
+      use: { ...devices['Desktop Chrome'], headless: isCI },
     },
 
     // 1. UI Mode (Firefox)
     {
       name: 'ui-firefox',
       testDir: './tests/ui/',
-      use: { ...devices['Desktop Firefox'], headless: true },
+      use: { ...devices['Desktop Firefox'], headless: isCI },
     },
 
     // 2. API Mode
